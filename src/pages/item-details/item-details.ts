633b40a16e4c545d50828f8275a504aeee52704c
sila <simon.lawrence@desynit.com>
2018-10-09 16:31:05 +0100

    first ion grids and actions

---
+++ src/pages/item-details/item-details.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+import {NavController, NavParams} from 'ionic-angular';
+
+@Component({
+  selector: 'item-details',
+  templateUrl: 'item-details.html'
+})
+export class ItemDetails {
+
+    
+}
