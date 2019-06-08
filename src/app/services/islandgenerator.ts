be6bd74e8261a839e6e59b806aed7d093b305169
simon <simon.lawrence@desynit.com>
2019-06-08 23:01:11 +0100

    inventory automation v1

--- src/app/services/islandgenerator.ts
+++ src/app/services/islandgenerator.ts
@@ -9,6 +9,22 @@ export class IslandGenerator {
     private easyland: {} = { "woods" : 40, "grass" : 40, "water" : 20 };
     private easycoast: {} = { "beach" : 90, "cliffs" : 10};
 
+    private landitems: {} = {"woods" : [ { "item" : "log", "chance" : 100, "min" : 3, "max" : 6 },
+                                         { "item" : "wood", "chance" : 100, "min" : 1, "max" : 6 },
+                                         { "item" : "vine", "chance" : 50, "min" : 0, "max" : 3 },
+                                         { "item" : "tinder", "chance" : 75, "min" : 1, "max" : 6 } ],
+                             "grass" : [ { "item" : "wood", "chance" : 80, "min" : 0, "max" : 2 },
+                                         { "item" : "long grass", "chance" : 100, "min" : 0, "max" : 3 },
+                                         { "item" : "clay", "chance" : 50, "min" : 1, "max" : 2 },
+                                         { "item" : "tinder", "chance" : 75, "min" : 1, "max" : 6 } ],
+                             "water" : [ { "item" : "clay", "chance" : 50, "min" : 1, "max" : 6 },
+                                         { "item" : "long grass", "chance" : 50, "min" : 1, "max" : 2 } ],                                         
+                           };
+
+    private coastitems: {} = {"beach" : [ {"item" : "wood", "chance" : 50, "min" : 1, "max" : 2} ],
+                              "cliffs" : [ {"item" : "rock", "chance" : 100, "min" : 1, "max" : 10} ]
+                             };
+
     constructor() {
         this.landarray = new Array<string>();
         this.coastarray = new Array<string>();
@@ -32,12 +48,30 @@ export class IslandGenerator {
 
     public generatelandtile(x: number, y: number): Tile {
         var landtype = this.landarray[Math.floor(Math.random() * Math.floor(100))];
-        return new Tile(x+''+y, x, y, landtype, [ { item : "log", qty : 4 }, { item : "tinder", qty : 5 }, { item : "rope", qty : 3 } ]);
+        var tileinventory = [];
+        for(var thisitem of this.landitems[landtype]) {
+            if((Math.random() * 100) < thisitem.chance) {
+                var qty = Math.floor(Math.random() * (thisitem.max+1 - thisitem.min) + thisitem.min);
+                if(qty > 0) {
+                    tileinventory.push({ item : thisitem.item, qty : qty});
+                }
+            }
+        }
+        return new Tile(x+''+y, x, y, landtype, tileinventory);
     }
 
     public generatecoasttile(x: number, y: number): Tile {
         var coasttype = this.coastarray[Math.floor(Math.random() * Math.floor(100))];
-        return new Tile(x+''+y, x, y, coasttype, [ { item : "log", qty : 4 }, { item : "tinder", qty : 5 }, { item : "rope", qty : 3 } ]);
+        var tileinventory = [];
+        for(var thisitem of this.landitems[coasttype]) {
+            if((Math.random() * 100) < thisitem.chance) {
+                var qty = Math.floor(Math.random() * (thisitem.max+1 - thisitem.min) + thisitem.min);
+                if(qty > 0) {
+                    tileinventory.push({ item : thisitem.item, qty : qty});
+                }
+            }
+        }
+        return new Tile(x+''+y, x, y, coasttype, tileinventory );
     }
 
 }
