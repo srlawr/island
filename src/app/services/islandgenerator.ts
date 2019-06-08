9aba5ead1b0417b2ec031af33f462783c7aba247
simon <simon.lawrence@desynit.com>
2019-06-08 09:17:50 +0100

    tile gen

--- src/app/services/islandgenerator.ts
+++ src/app/services/islandgenerator.ts
@@ -3,13 +3,41 @@ import { Tile } from '../models/tile';
 
 export class IslandGenerator {
 
+    private landarray: string[];
+    private coastarray: string[];
+
+    private easyland: {} = { "woods" : 40, "grass" : 40, "water" : 20 };
+    private easycoast: {} = { "beach" : 90, "cliffs" : 10};
+
     constructor() {
+        this.landarray = new Array<string>();
+        this.coastarray = new Array<string>();
+        var count: number = 0;
+
+        for(var property in this.easyland) {
+            for(var clicker = 0; clicker < this.easyland[property]; clicker++) {
+                this.landarray.push(property);
+                console.log(property);
+            }
+        }
+        for(var property in this.easycoast) {
+            for(var clicker = 0; clicker < this.easycoast[property]; clicker++) {
+                this.coastarray.push(property);
+                console.log(property);
+            }
+        }
+        console.log(this.landarray[Math.floor(Math.random() * Math.floor(100))]);
+        console.log(this.coastarray[Math.floor(Math.random() * Math.floor(100))]);
     }
 
+    public generatelandtile(x: number, y: number): Tile {
+        var landtype = this.landarray[Math.floor(Math.random() * Math.floor(100))];
+        return new Tile(x+''+y, x, y, landtype, [ { item : "log", qty : 4 }, { item : "tinder", qty : 5 }, { item : "rope", qty : 3 } ]);
+    }
 
     public generatecoasttile(x: number, y: number): Tile {
-
-        return new Tile(x+''+y, x, y, 'beach',  [ { item : "log", qty : 4 }, { item : "tinder", qty : 5 }, { item : "rope", qty : 3 } ]);
+        var coasttype = this.coastarray[Math.floor(Math.random() * Math.floor(100))];
+        return new Tile(x+''+y, x, y, coasttype, [ { item : "log", qty : 4 }, { item : "tinder", qty : 5 }, { item : "rope", qty : 3 } ]);
     }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,34 +26,34 @@ export class Home {
 
  
       this.storage.set("grid", { "00": islandgenerator.generatecoasttile(0, 0),
-                            "01": new Tile("01", 0, 1, "beach", [ { item : "vine", qty : 8 } ]),
-                            "02": new Tile("02", 0, 2, "beach", [ { item : "sand", qty : 1000 }, { item : "salt", qty : 1000 } ]),
-                            "03": new Tile("03", 0, 3, "beach", [ { item : "wood", qty :3 } ]),
-                            "04": new Tile("04", 0, 4, "beach", []),
+                            "01": islandgenerator.generatecoasttile(0, 1),
+                            "02": islandgenerator.generatecoasttile(0, 2),
+                            "03": islandgenerator.generatecoasttile(0, 3),
+                            "04": islandgenerator.generatecoasttile(0, 4),
                         
-                            "10": new Tile("10", 1, 0, "beach", []),
-                            "11": new Tile("11", 1, 1, "woods", []),
-                            "12": new Tile("12", 1, 2, "woods", []),
-                            "13": new Tile("13", 1, 3, "grass", []),
-                            "14": new Tile("14", 1, 4, "cliffs", []),
+                            "10": islandgenerator.generatecoasttile(1, 0),
+                            "11": islandgenerator.generatelandtile(1, 1),
+                            "12": islandgenerator.generatelandtile(1, 2),
+                            "13": islandgenerator.generatelandtile(1, 3),
+                            "14": islandgenerator.generatecoasttile(1, 4),
 
-                            "20": new Tile("10", 2, 0, "beach", []),
-                            "21": new Tile("11", 2, 1, "woods", []),
+                            "20": islandgenerator.generatecoasttile(2, 0),
+                            "21": islandgenerator.generatelandtile(2, 1),
                             "22": new Tile("12", 2, 2, "hut", []),
-                            "23": new Tile("13", 2, 3, "water", []),
-                            "24": new Tile("14", 2, 4, "beach", []),
+                            "23": islandgenerator.generatelandtile(2, 3),
+                            "24": islandgenerator.generatecoasttile(2, 4),
 
-                            "30": new Tile("15", 3, 0, "beach", []),
-                            "31": new Tile("16", 3, 1, "woods", []),
-                            "32": new Tile("17", 3, 2, "grass", []),
-                            "33": new Tile("18", 3, 3, "water", []),
-                            "34": new Tile("19", 3, 4, "beach", []),
+                            "30": islandgenerator.generatecoasttile(3, 0),
+                            "31": islandgenerator.generatelandtile(3, 1),
+                            "32": islandgenerator.generatelandtile(3, 2),
+                            "33": islandgenerator.generatelandtile(3, 3),
+                            "34": islandgenerator.generatecoasttile(0, 4),
 
-                            "40": new Tile("20", 4, 0, "beach", []),
-                            "41": new Tile("21", 4, 1, "beach", []),
-                            "42": new Tile("22", 4, 2, "beach", []),
+                            "40": islandgenerator.generatecoasttile(4, 0),
+                            "41": islandgenerator.generatecoasttile(4, 1),
+                            "42": islandgenerator.generatecoasttile(4, 2),
                             "43": new Tile("23", 4, 3, "jetty", []),
-                            "44": new Tile("24", 5, 3, "beach", [])
+                            "44": islandgenerator.generatecoasttile(4, 4)
                           });
     });
   }
